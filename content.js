b139e2082af7c47e55e939a2d3e6420c4ed7114a
AppleTax <55190549+Apple-Tax@users.noreply.github.com>
2022-06-05 08:45:40 +0800

    Multichoice functionality

--- content.js
+++ content.js
@@ -1,18 +1,32 @@
 'use strict';
 
 
-let data = chrome.storage.local;
+const data = chrome.storage.local;
+const cmid = detectURL()[1];
 
-if (isReview()) {
-    data.set(stripAns());
+// left for debug purposes
+// console.log(`Test: ${isTest()}, Review: ${isReview()}, Attemp: ${isAttemp()}`);
+// console.log(getData(cmid));
+
+if (isTest()) {
+    if (isReview()) data.set(stripTestAns());
+    else if (isAttemp()) {
+        getData(cmid).then( val => {
+            fillTestAns(val);
+        })
+    }
 }
-
-if (isAttemp()) {
-    let cmid = detectURL()[1];
-    getData(cmid).then((val) => {
-        console.log(val);
-        if (val != undefined) fillAns(val);
-    })
+else {
+    if (isReview()) {
+        data.set(stripAns());
+    }
+    
+    else if (isAttemp()) {
+        getData(cmid).then((val) => {
+            console.log(val);
+            if (val != undefined) fillAns(val);
+        })
+    }
 }
 
 // https://stackoverflow.com/questions/37700051/chrome-extension-is-there-any-way-to-make-chrome-storage-local-get-return-so
@@ -41,8 +55,8 @@ function detectURL(){
         return [undefined, undefined]
     }
     let type = url.pathname.split('/')[3].split('.')[0]
-    let cmid = url.searchParams.get("cmid")
-    return [type, cmid]
+    let _cmid = url.searchParams.get("cmid")
+    return [type, _cmid]
 }
 
 function isAttemp() {
@@ -55,7 +69,16 @@ function isReview() {
     return res[0] == 'review' && res[1] != null ? true : false;
 }
 
+function isTest() {
+    if (document.getElementsByClassName('que multichoice').length == 0) return false
+    return true
+}
+
 function stripAns() {
+    function process(rawAns) {
+        var exp = /(答錯)|(答對)|(沒被回答)|(正確答案是：)|(得分 \d\/配分\d)/g
+        return rawAns.replace(exp, '');
+    }
     var ansBox = document.getElementsByClassName("feedbackspan yui3-overlay-content yui3-widget-stdmod");
     var ans = [];
 
@@ -63,12 +86,24 @@ function stripAns() {
         ans.push(process(item.textContent));
     }
     
+
+    return JSON.parse(`{"${cmid}":${JSON.stringify(ans)}}`);
+}
+
+function stripTestAns() {
     function process(rawAns) {
-        var exp = /(答錯)|(答對)|(沒被回答)|(正確答案是：)|(得分 \d\/配分\d)/g
+        let exp = /(回饋)|(你答錯了!)|(沒被回答)|(正確答案：)/g
         return rawAns.replace(exp, '');
     }
 
-    let cmid = detectURL()[1].toString();
+    var ans = []
+    for (const element of document.getElementsByClassName('rightanswer')) {
+        ans.push(process(element.textContent));
+    }
+
+    // c 
+    console.log(ans);
+
     return JSON.parse(`{"${cmid}":${JSON.stringify(ans)}}`);
 }
 
@@ -86,5 +121,20 @@ function fillAns(Answers) {
             AnsBox.childNodes.forEach(element => {if (element.textContent == ans) val = element.value;});
             AnsBox.value = val;
         }
+    }
+}
+
+function fillTestAns(Answers) {
+    let ansBoxes = document.getElementsByClassName('que multichoice');
+    for (let ansBox of ansBoxes) {
+        let idx = Number(ansBox.getElementsByClassName('qno')[0].textContent)-1;
+        for (let eachAns of ansBox.getElementsByClassName('flex-fill ml-1')) {
+            // c
+            console.log(`ans: ${eachAns.textContent}, Stored: ${Answers[idx]}`)
+            if (eachAns.textContent == Answers[idx]) {
+                eachAns.parentElement.parentElement.getElementsByTagName('input')[0].checked = true;
+            }
+        }
+
     }
 }
